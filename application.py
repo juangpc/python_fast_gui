78d5b0880c0d0f0f9380889cdda15818a5fcea24
juangpc <juangpc@gmail.com>
2021-07-29 10:17:45 -0400

    update fixes

--- application.py
+++ application.py
@@ -1,1 +1,17 @@
-import fast_gui
+import fast_gui.app
+
+def start():
+    pass
+    
+def stop():
+    pass
+
+def void():
+    pass
+
+      
+app = fast_gui.app.App()
+app.set_menu(['START', 'STOP', 'RESTART', 'EXIT'])
+app.set_callbacks([start, stop, void, app.exit])
+
+app.start()
